416d2e1badc19f50e210f72b9cc2f1e1fc3a05ef
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-10-10 10:58:14 +0300

    verify

--- connect.py
+++ connect.py
@@ -3,6 +3,11 @@ import redis
 pool = redis.ConnectionPool(host='localhost', port=6379, username="default", password="password123", db=0)
 redis = redis.Redis(connection_pool=pool)
 
+if redis.ping():
+    print("Connexion reussi")
+else:
+    print("Erreur de connexion")
+
 def setValue(name, value):
     redis.set(name, value)
 
